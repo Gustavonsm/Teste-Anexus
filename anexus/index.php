e2cbddb97571faad11912bc495883721e5588506
Gustavonsm <72533341+Gustavonsm@users.noreply.github.com>
2020-10-08 18:57:34 -0300

    Update index.php

--- anexus/index.php
+++ anexus/index.php
@@ -21,7 +21,7 @@
       }
 
       @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
+]{
           font-size: 3.5rem;
         }
       }
@@ -40,46 +40,60 @@
 
   <main role="main" class="inner cover">
   	<form name="Cadastro" method="POST">
-    	<h1>Verificar pontos do usuário</h1>
-
+    	<h1>Verificar pontos do usuário</h1><br>
+    <td><label>Usuário 2</label></td>
+    <td><input type="number" name="user2pontos" placeholder="Pontos" class="txtbox"></td>  <br>
+    <td><label>Usuário 3</label></td>
+    <td><input type="number" name="user3pontos" placeholder="Pontos" class="txtbox"></td>  <br><br>
  		<br><input name="btncalcular" type="submit" value="Calcular" class="btn btn-lg btn-secondary"><br><br>
  		<?php
 
+      $usuario2= filter_input(INPUT_POST, 'user2pontos' , FILTER_SANITIZE_STRING);
+      $usuario3= filter_input(INPUT_POST, 'user3pontos' , FILTER_SANITIZE_STRING);
  			$botao= filter_input(INPUT_POST, 'btncalcular' , FILTER_SANITIZE_STRING);
 
  			if($botao == "Calcular")
  			{
-				 $valor1 = 0;
-			     $valor2 = null;
-			     $valor3 = null;
-			     $value = null;	
+		    if($usuario2 && $usuario3)
+        {
+          if($usuario3 > $usuario2)
+          {
+              $valoresquerdo = $usuario3;
+          }
+          else
+          {
+            $valoresquerdo = $usuario2; 
+          }
+          if($usuario3 < $usuario2)
+          {
+            $valordireito = $usuario3;
+          }
+          else
+          {
+            $valordireito = $usuario2 ;
+          }
+            
+          $total = $usuario2 +$usuario3;
 
-
-					
-				if($valor3 == null)
-				{
-					$valor3 = 100;
-				}
-				else
-				{
-					$value = $valor2;
-				}
-				if($valor2 == null)
-				{
-					$valor2 = 200;
-				}
-				else
-				{
-					$value = $valor3	;
-				}
-					
-				$total = $valor2 +$valor3;
-
-				echo '<center><table>
-        <tr><td>O Usuário um tem no total:</td><td> '.$total.'</td></tr>
-						<tr><td>Pontos do laddo esquerdo: </td><td> ' . $valor2 .  '</td></tr> 
-            <tr> <td>Pontos do lado direito:</td> <td> ' . $valor3 .'</td></tr>
-            </table></center>';	
+          echo '<center><table>
+          <tr><td><h5 style="color: black;">O Usuário um tem no total:</h5></td><td><h5 style="color: black;"> '.$total.'</h5></td></tr> 
+          <tr> <td><h5 style="color: black;">Pontos do Usuário 2:</h5></td> <td> <h5 style="color: black;">' . $usuario2 .'</h5></td></tr>
+              <tr> <td><h5 style="color: black;">Pontos do Usuário 3:</h5> </td> <td><h5 style="color: black;"> ' . $usuario3 .'</h5></td></tr>
+              <tr><td></td><td></td></tr>
+              <tr><td></td><td></td></tr>
+              <tr><td><h5>Pontos do laddo esquerdo:</h5></td><td><h5> ' . $valoresquerdo .  '</h5></td></tr> 
+              <tr><td><h5>Pontos do lado direito:</h5></td> <td> <h5>' . $valordireito .'</h5></td></tr>
+             
+              </table></center>'; 
+        }
+        else
+        {
+          echo '<h3 style="color: red;
+           text-shadow: 0 .2rem .2rem rgba(0, 0, 0, 10);">';
+          echo 'Preencha todos os campos!!';
+          echo '</h3>';
+        }
+				
 			}	
 		?>
 	</form>   
